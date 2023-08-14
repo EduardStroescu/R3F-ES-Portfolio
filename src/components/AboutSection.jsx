61febe596aba217f80eab57c1744aded223b8a48
Eduard Stroescu <124309370+EduardStroescu@users.noreply.github.com>
2023-08-15 02:50:22 +0300

    Content change for the About Section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -22,12 +22,12 @@ export default function AboutSection({
       fullscreen
       transform
       scale={0.5}
-      position={[11, 6, 15]}
+      position={[11, 5.5, 15]}
       wrapperClass="m-0 p-0 box-border text-white overflow-hidden h-full w-full"
     >
       <a.div
         style={{ clipPath, pointerEvents: "none", transform: "scale(2)" }}
-        className="MainFont z-[-5] mx-[0px] sm:mx-[150px] md:mx-[200px] lg:mx-[30%] 2xl:mx-[35%] flex flex-col justify-center items-center gap-6 bg-[#10D9E182]/[0.80] rounded-xl border border-[#10D9E182]/[1] bg-gradient-to-t from-[#11e8bb]/[0.6] to-[#8200c9]/[0.7]"
+        className="MainFont z-[-5] mx-[-5px] sm:mx-[150px] md:mx-[200px] lg:mx-[30%] 2xl:mx-[35%] flex flex-col justify-center items-center gap-4 rounded-xl border border-[#10D9E182] bg-gradient-to-t from-[#11e8bb]/[0.95] to-[#8200c9]/[0.95]"
       >
         <button
           style={{ pointerEvents: visible ? "auto" : "none" }}
@@ -40,22 +40,33 @@ export default function AboutSection({
         >
           &#10094; Back
         </button>
-        <h1 className="font-bold titleColor text-4xl pb-6 mt-[-20px]">About</h1>
-        <p className="py-0 px-[15px] text-lg text-justify indent-5">
+        <h1 className="font-bold titleColor text-4xl pb-4 mt-[-30px]">About</h1>
+        <p className="py-0 px-[15px] text-md text-justify indent-5">
           I&apos;m Eduard, a Front-End developer specializing in web-based
-          technologies and frameworks. I am driven by an insatiable passion for
-          learning, which keeps me at the forefront of technological
-          advancements. My commitment to continuous improvement ensures that my
-          skills remain sharp and effective.
+          technologies and frameworks. In my pursuit of excellence, I am driven
+          by a profound passion for both creativity and functionality. I believe
+          that design and user experience are two sides of the same coin, and my
+          goal is to seamlessly blend them to create interfaces that leave a
+          lasting impact on users.
         </p>
-
-        <p className="py-[15px] px-[15px] text-justify indent-5">
-          If you&apos;re searching for a versatile and dedicated developer to
-          strengthen your team, I invite you to explore my portfolio. With a
-          wealth of knowledge and a forward-thinking approach, I am ready to
-          tackle new challenges and create innovative solutions together.
-          Let&apos;s connect and uncover the exciting possibilities that lie
-          ahead!
+        <p className="px-[15px] text-md text-justify indent-5">
+          My dedication to self-improvement serves as the backbone of my career.
+          Staying ahead in the fast-paced world of technology is my aim, and I
+          am relentless in my pursuit of mastering the latest trends and
+          techniques. This unyielding commitment ensures that my skills are
+          always finely honed, enabling me to deliver top-notch results with
+          every project.
+        </p>
+        {/* The fusion of aesthetic brilliance and seamless functionality is where I excel, as I aim to not just meet, but exceed user expectations. */}
+        <p className=" px-[15px] text-md text-justify indent-5">
+          With a forward-thinking approach, I am ready to tackle new challenges
+          and create innovative solutions together. If you&apos;re searching for
+          a versatile and dedicated developer to strengthen your team, I invite
+          you to explore my portfolio.
+        </p>
+        <p className="pb-8 px-[15px] text-lg text-center">
+          Let&apos;s connect and craft digital experiences that make a lasting
+          impact!
         </p>
       </a.div>
     </Html>
